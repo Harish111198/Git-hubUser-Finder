4305067fc759f5e66bd7ff95ea2140215540ba50
Harish <harishk001998@gmail.com>
2022-04-04 08:48:24 +0530

    modified spinner

---
+++ src/components/Spinner.jsx
@@ -0,0 +1,6 @@
+import React, { Fragment } from 'react'
+import spinner from '../assets/images/spinner.gif'
+const Spinner = () => <Fragment>
+  <img src={spinner} alt="Loading..." style={{ width: '200px', margin: 'auto', display: 'block' }}></img>
+</Fragment>
+export default Spinner
--- src/components/Users.jsx
+++ src/components/Users.jsx
@@ -1,24 +1,38 @@
 import React, { Component } from 'react'
 import UserItem from "../components/UserItem"
+import Spinner from './Spinner'
 
-class Users extends Component {
 
-
-  render() {
-    const styleItem = {
-      display: 'grid',
-      gridTemplateColumns: 'repeat(3,1fr)',
-      gridGap: '1rem'
-    }
+const Users = ({ users, loading }) => {
+  if (loading) {
+    return <Spinner />
+  }
+  else {
     return (
-      <div style={styleItem}>
-        {this.props.users.map(user => (
+      <div style={userStyle}>
+        {users.map(user => (
           < UserItem key={user.id} user={user} />
         ))}
       </div>
 
     )
+
   }
+  Users.prototypes = {
+    users: PropTypes.array.isRequired,
+    loading: PropTypes.bool.isRequired
+
+  }
+
+
+
+
+
+}
+const userStyle = {
+  display: 'grid',
+  gridTemplateColumns: 'repeat(3,1fr)',
+  gridGap: '1rem'
 }
 
 export default Users
